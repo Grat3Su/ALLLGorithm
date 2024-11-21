22dae6020b616cff2e18a705e4aa17ea6523ed0c
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2024-11-22 03:57:37 +0900

    Create 중복빼고정렬하기.cpp

---
+++ Cabbage/Code/중복빼고정렬하기.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> v(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
