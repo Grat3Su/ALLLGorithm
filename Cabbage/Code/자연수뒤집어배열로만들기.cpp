b6ebd061e2fd8ae8bf856558c9775f6001c3bd3b
건강 보조 식품 양배추 <49223403+healthcabbage@users.noreply.github.com>
2024-12-16 21:31:35 +0900

    Create 자연수뒤집어배열로만들기.cpp

---
+++ Cabbage/Code/자연수뒤집어배열로만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    while (true)
+    {
+        answer.push_back(n%10);
+        n = n/10;
+        if (n==0)
+        {
+            break;
+        }
+    }
+    return answer;
+}
